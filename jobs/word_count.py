045eabe417e41fa6c90242abb7ada6fc86a08ed3
Harshit Tripathi <harshittripathi001@gmail.com>
2024-04-28 23:11:13 +0530

    added spark and dag

---
+++ jobs/word_count.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("WordCount").getOrCreate()
+
+text = "Hello Spark Hello Python Hello Airflow Hello Docker and Hello Harshit"
+
+words = spark.sparkContext.parallelize(text.split(" "))
+
+wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+for wc in wordCounts.collect():
+    print(wc[0], wc[1])
+
+spark.stop()
